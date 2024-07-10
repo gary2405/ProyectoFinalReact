a0100827b89e9fb06cac29ad1c727ece236d0097
gary2504 <garychaves25@gmail.com>
2024-07-10 15:00:13 -0600

    implementacion de Put para editar productos junto a un  boton que cambiara cada que quiera editar

---
+++ proyectoReact/src/services/productosput.jsx
@@ -0,0 +1,15 @@
+const productosPUT = async (id, nombre, precio, ingredientes, imgurl) => {
+    const actualizadoProducto = { nombre, precio, ingredientes, imgurl };
+    try {
+      const response = await fetch(`http://localhost:3002/productos/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(actualizadoProducto)
+      });
+      return response.ok ? await response.json() : null;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+  export default productosPUT; 
